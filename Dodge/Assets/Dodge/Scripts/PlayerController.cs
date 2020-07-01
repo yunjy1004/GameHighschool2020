e784cee38572568eaf3520ae13736a68b66df66e
seongjun yun <yunjy104@gmail.com>
2020-07-01 19:38:55 +0900

    진우

---
+++ Dodge/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_speed = 25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /*gameObject*/GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxis, 0, yAxis) * m_speed);
+
+        float fireAxis = Input.GetAxis("Fire1");
+
+        if (fireAxis > 0.95f)
+            Die();
+
+        //if (Input.GetAxis("Horizontal"))
+        //{
+        //    //transform.position += Vector3.left * m_speed * Time.deltaTime;
+        //    rigidbody.AddForce(Vector3.left * m_speed);
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    rigidbody.AddForce(Vector3.right * m_speed);
+        //}
+        //else if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    rigidbody.AddForce(Vector3.forward * m_speed);
+        //}
+        //else if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    rigidbody.AddForce(Vector3.back * m_speed);
+        //}
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dodge/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Test : EditorWindow
+{
+    //Add menu named "My Window" to the Window menu
+    [MenuItem("Window/Test")]
+
+    static void Init()
+    {
+        //Get existing open window or if none, make a new one:
+        Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        Handles.color = Color.red;
+        Handles.DrawRectangle(0, new Vector3(200, 200, 0), Quaternion.identity, 100);
+        Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100);
+        Handles.DrawSolidDisc(new Vector3(150, 150, 0), Vector3.forward, 50);
+        Handles.DrawSolidDisc(new Vector3(250, 150, 0), Vector3.forward, 50);
+
+    }
+}
