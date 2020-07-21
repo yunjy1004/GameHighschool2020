1fab26dd46042f6130bfb33c8c1a5a52b839a163
seongjun yun <yunjy104@gmail.com>
2020-07-21 21:25:14 +0900

    07/21

---
+++ ShootingGame/Assets/Shooting Game/Scripts/EnemySmall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySmall : MonoBehaviour
+{
+    public float m_Speed = 3f;
+
+    public float m_LifeTime = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = transform.up * m_Speed;
+        Vector3 movement = velocity * Time.deltaTime;
+        transform.position -= movement;
+
+        if (m_LifeTime <= 0)
+            Destroy(gameObject);
+
+        m_LifeTime -= Time.deltaTime;
+        
+    }
+}
