09fa816c66e527b79f63d61627b3f1ba9f6c2bb3
seongjun yun <yunjy104@gmail.com>
2020-07-23 21:19:22 +0900

    07/23 애니매이션 공부

---
+++ AnimationTest/Assets/AnimationTest/Scripts/Blackholl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackholl : MonoBehaviour
+{
+    public float Delay = 3f;
+    public float CoolTime = 0f;
+
+    private void Update()
+    {
+        if (CoolTime >= Delay)
+            GetComponent<Animator>().SetTrigger("Die");
+
+        CoolTime += Time.deltaTime;
+    }
+
+}
---
+++ AnimationTest/Assets/AnimationTest/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator m_Animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_Animator.SetTrigger("Attack");
+        }
+    }
+}
