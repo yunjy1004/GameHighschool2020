bd51f7069e89e4e0ec20cd00caddfe9c76e755ed
seongjun yun <yunjy104@gmail.com>
2020-09-02 21:22:25 +0900

    사다리타는 거 만듬

--- SunnyLand/Assets/GameSchool/Scripts/PlayController.cs
+++ SunnyLand/Assets/GameSchool/Scripts/PlayController.cs
@@ -9,6 +9,7 @@ public class PlayController : MonoBehaviour
     private Rigidbody2D m_Rigidbody2D;
     private Animator m_Animator;
 
+    public float m_ClimbSpeed = 3f;
     public float m_XAxisSpeed = 3f;
     public float m_YJumpPower = 3f;
     public float m_Speed = 50f;
@@ -16,6 +17,8 @@ public class PlayController : MonoBehaviour
     public int m_JumpCount = 0;
 
     public bool m_IsJumping = false;
+    public bool m_IsTouchLadder = false;
+    public bool m_IsClimbing = false;
 
     protected void Start()
     {
@@ -32,37 +35,66 @@ public class PlayController : MonoBehaviour
         velocity.x = xAxis * m_XAxisSpeed;
         m_Rigidbody2D.velocity = velocity;
 
-        if (xAxis > 0)
-            m_Sprite.localScale = new Vector3(1, 1, 1);
-        else if (xAxis < 0)
-            m_Sprite.localScale = new Vector3(-1, 1, 1);
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) && m_JumpCount <= 0)
+        if (m_IsTouchLadder && Mathf.Abs(yAxis) > 0.5f)
         {
-            m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
-            m_JumpCount++;
+            m_IsClimbing = true;
         }
 
-        //이거 추가
-        m_IsJumping = Mathf.Abs(velocity.y) >= 0.5f ? true : false;
+        if (!m_IsClimbing)
+        {
 
-        //스크립트 2줄 작성
-        m_Animator.SetBool("isJumping", m_IsJumping);
-        m_Animator.SetFloat("Velocity X", Mathf.Abs(xAxis));
+            if (xAxis > 0)
+                m_Sprite.localScale = new Vector3(1, 1, 1);
+            else if (xAxis < 0)
+                m_Sprite.localScale = new Vector3(-1, 1, 1);
 
-        //이거 추가
-        m_Animator.SetFloat("Velocity Y", velocity.y);
+            if (Input.GetKeyDown(KeyCode.UpArrow) && m_JumpCount <= 0)
+            {
+                m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
+                m_JumpCount++;
+            }
 
-        //var animator = GetComponent<Animator>();
-        //animator.SetFloat("Velocity Y", velocity.y);
+            //이거 추가
+            m_IsJumping = Mathf.Abs(velocity.y) >= 0.5f ? true : false;
+
+            //스크립트 2줄 작성
+            m_Animator.SetBool("isJumping", m_IsJumping);
+            m_Animator.SetFloat("Velocity X", Mathf.Abs(xAxis));
+
+            //이거 추가
+            m_Animator.SetFloat("Velocity Y", velocity.y);
+
+            //var animator = GetComponent<Animator>();
+            //animator.SetFloat("Velocity Y", velocity.y);
+        }
+         else
+        {
+
+            m_Rigidbody2D.constraints = m_Rigidbody2D.constraints | RigidbodyConstraints2D.FreezePosition;
+
+            Vector3 movement = Vector3.zero;
+            movement.x = xAxis * m_ClimbSpeed * Time.deltaTime;
+            movement.y = yAxis * m_ClimbSpeed * Time.deltaTime;
+
+            transform.position += movement;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ClimbingExit();
+            }
+        }
+
+        //Update 사다리타기 점프 캔슬하는데
+        m_Animator.SetBool("IsClimbing", m_IsClimbing);
+        m_Animator.SetFloat("ClimbSpeed", Mathf.Abs(xAxis) + Mathf.Abs(yAxis));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        foreach(ContactPoint2D contact in collision.contacts)
+        foreach (ContactPoint2D contact in collision.contacts)
         {
             Debug.DrawRay(contact.point, contact.normal, Color.white);
-            if(contact.normal.y > 0.5f)
+            if (contact.normal.y > 0.5f)
             {
                 m_JumpCount = 0;
             }
@@ -71,6 +103,31 @@ public class PlayController : MonoBehaviour
         {
             m_JumpCount = 0;
         }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = false;
+            ClimbingExit();
+        }
+    }
+
+    private void ClimbingExit()
+    {
+        m_Rigidbody2D.constraints = m_Rigidbody2D.constraints & ~RigidbodyConstraints2D.FreezePosition;
+        m_IsClimbing = false;
+
+        m_Animator.SetBool("IsClimbing", m_IsClimbing);
     }
 
     //private void OnTriggerStay2D(Collider2D collision)
