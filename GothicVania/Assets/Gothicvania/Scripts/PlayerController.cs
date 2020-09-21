865ca59151bc76e37855d579f17cd93002f00cd9
seongjun yun <yunjy104@gmail.com>
2020-09-21 21:22:27 +0900

    복습함 플레이어 이동

---
+++ GothicVania/Assets/Gothicvania/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+
+        float xMovement = Speed * xAxis * Time.deltaTime;
+        Vector3 movement = Vector3.right * xMovement;
+        transform.position += movement;
+
+    }
+}
