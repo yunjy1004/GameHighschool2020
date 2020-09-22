3c001c9afa14a186d3dbb41e0bb8d604e3df4bd0
seongjun yun <yunjy104@gmail.com>
2020-09-22 21:13:30 +0900

    복습게임 움직임 및 이상 상태 state변화 다룸

--- GothicVania/Assets/Gothicvania/Scripts/PlayerController.cs
+++ GothicVania/Assets/Gothicvania/Scripts/PlayerController.cs
@@ -2,18 +2,155 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 1f;
-    // Update is called once per frame
+    public enum State
+    {
+        None,
+        GroundWalk,
+        Jump,
+        Attack,
+        Hurt
+    }
+
+    public State m_State = State.None;
+
+    public float m_CurrentSpeed = 1f;
+    public float m_CurrentJumpSpeed = 3f;
+
+    public SpriteRenderer m_Sprite;
+    public Rigidbody2D m_Rigidbody;
+
+    protected void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+        switch (m_State)
+        {
+            case State.None:
+                OnTick_None();
+                break;
+            case State.GroundWalk:
+                OnTick_GroundWalk();
+                break;
+            case State.Jump:
+                OnTick_Jump();
+                break;
+            default:
+                break;
+        }
+    }
 
+    public void OnTick_None()
+    {
+        if (m_IsGround)
+        {
+            m_State = State.GroundWalk;
+        }
+    }
+
+    public void OnTick_Jump()
+    {
         float xAxis = Input.GetAxis("Horizontal");
 
-        float xMovement = Speed * xAxis * Time.deltaTime;
+        float xMovement = m_CurrentSpeed * xAxis * Time.deltaTime;
         Vector3 movement = Vector3.right * xMovement;
         transform.position += movement;
 
+        if (xAxis < 0)
+        {
+            m_Sprite.flipX = true;
+        }
+        else if (xAxis > 0)
+        {
+            m_Sprite.flipX = false;
+        }
+
+        if (m_IsGround)
+            m_State = State.GroundWalk;
+    }
+
+    public void OnTick_GroundWalk()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        if (yAxis > 0)
+        {
+            m_State = State.Jump;
+        }
+
+        if (yAxis > 0)
+        {
+            Vector2 velocity = m_Rigidbody.velocity;
+            velocity.y = m_CurrentJumpSpeed;
+            m_Rigidbody.velocity = velocity;
+        }
+
+        if (xAxis < 0)
+        {
+            m_Sprite.flipX = true;
+        }
+        else if (xAxis > 0)
+        {
+            m_Sprite.flipX = false;
+        }
+
+        //트랜스폼으로 이산적으로 움직임
+        float xMovement = m_CurrentSpeed * xAxis * Time.deltaTime;
+        Vector3 movement = Vector3.right * xMovement;
+        transform.position += movement;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //foreach(ContactPoint2D contact 
+        //    in collision.contacts)
+        //{
+        //    if(contact.collider.tag =="Ground" 
+        //    && contact.otherCollider.tag == "WalkCollider")
+        //    {
+        //        if(contact.normal.y > 0.8f)
+        //        {
+        //            Debug.Log("캐릭터 바닥에 있음");
+
+        //            m_IsGround = true;
+        //        }
+        //    }
+        //}
+    }
+
+    public bool m_IsGround = false;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact
+            in collision.contacts)
+        {
+            if (contact.collider.tag == "Ground"
+            && contact.otherCollider.tag == "WalkCollider")
+            {
+                if (contact.normal.y > 0.8f)
+                {
+                    Debug.Log("캐릭터 바닥에 있음");
+
+                    m_IsGround = true;
+                }
+            }
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground"
+        && collision.otherCollider.tag == "WalkCollider")
+        {
+            m_IsGround = false;
+        }
     }
 }
