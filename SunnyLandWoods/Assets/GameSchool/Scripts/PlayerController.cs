1646f413471ead6e8a2ff5f931c7aa6799b2cd9a
seongjun yun <yunjy104@gmail.com>
2020-10-06 21:43:50 +0900

    써니랜드 두번째 작품 10/06 side 물리처리 판정 마리오처럼 블럭위로 점프기능

---
+++ SunnyLandWoods/Assets/GameSchool/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D m_Rigidbody2D;
+    public float m_MovementSpeed = 5f;
+    public float m_JumpSpeed = 10f;
+
+    private void FixedUpdate()
+    {
+        var xAxis = Input.GetAxis("Horizontal");
+        var yAxis = Input.GetAxis("Vertical");
+
+        Vector2 velocity = m_Rigidbody2D.velocity;
+        velocity.x = xAxis * m_MovementSpeed;
+
+        if (yAxis > 0.1f)
+        {
+            velocity.y = m_JumpSpeed;
+        }
+        m_Rigidbody2D.velocity = velocity;
+    }
+}
