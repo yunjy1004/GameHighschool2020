62fb2a22d9b2e040e084f67ce579af6b1ef928f6
seongjun yun <yunjy104@gmail.com>
2020-11-04 21:08:14 +0900

    3D IK배움, 맵 빛 렌더링 배움

---
+++ Zombie/Assets/Zombie/Property Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyTest : MonoBehaviour
+{
+    public float InputA
+    {
+        get; set;
+    }
+
+    public float InputB
+    {
+        private get; set;
+    }
+
+    private float m_InputC
+    {
+        get
+        {
+            return m_InputC + 10;
+        }
+
+        set
+        {
+            m_InputC = value = 10;
+        }
+    }
+}
