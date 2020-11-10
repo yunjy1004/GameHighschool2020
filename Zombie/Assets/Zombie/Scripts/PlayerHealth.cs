3d993c351e870957c50f9ce15b306130fc7b05df
seongjun yun <yunjy104@gmail.com>
2020-11-10 21:16:13 +0900

    플레이어 체렉바 만들어서 블러드 이펙트 보여주고 체력빠지는거 만듬

--- Zombie/Assets/Zombie/Scripts/PlayerHealth.cs
+++ Zombie/Assets/Zombie/Scripts/PlayerHealth.cs
@@ -2,8 +2,10 @@
 using UnityEngine.UI; // UI 관련 코드
 
 // 플레이어 캐릭터의 생명체로서의 동작을 담당
-public class PlayerHealth : LivingEntity {
-    public Slider healthSlider; // 체력을 표시할 UI 슬라이더
+public class PlayerHealth : LivingEntity
+{
+    public Image healthSlider;
+    // public Slider healthSlider; // 체력을 표시할 UI 슬라이더
 
     public AudioClip deathClip; // 사망 소리
     public AudioClip hitClip; // 피격 소리
@@ -15,34 +17,65 @@ public class PlayerHealth : LivingEntity {
     private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
     private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
 
-    private void Awake() {
+    public GameObject bloodEffect;
+
+    private void Awake()
+    {
         // 사용할 컴포넌트를 가져오기
     }
 
-    protected override void OnEnable() {
+    protected override void OnEnable()
+    {
         // LivingEntity의 OnEnable() 실행 (상태 초기화)
         base.OnEnable();
+
+        RefreshHP();
     }
 
     // 체력 회복
-    public override void RestoreHealth(float newHealth) {
+    public override void RestoreHealth(float newHealth)
+    {
         // LivingEntity의 RestoreHealth() 실행 (체력 증가)
         base.RestoreHealth(newHealth);
+
+        RefreshHP();
     }
 
     // 데미지 처리
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
         // LivingEntity의 OnDamage() 실행(데미지 적용)
         base.OnDamage(damage, hitPoint, hitDirection);
+
+        var effect = Instantiate(bloodEffect);
+        effect.transform.position = hitPoint;
+        effect.transform.forward = hitDirection;
+        effect.GetComponent<ParticleSystem>().Play();
+        RefreshHP();
     }
 
     // 사망 처리
-    public override void Die() {
+    public override void Die()
+    {
         // LivingEntity의 Die() 실행(사망 적용)
         base.Die();
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+    }
+
+    private void RefreshHP()
+    {
+        healthSlider.fillAmount = health / startingHealth;
+    }
+
+    [ContextMenu("TestHit")]
+    public void TestHit()
+    {
+        OnDamage(10,
+            transform.position + Vector3.up,
+            transform.forward);
     }
 }
