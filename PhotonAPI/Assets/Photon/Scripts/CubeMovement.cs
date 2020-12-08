8aefb345bcb4f824cd50c80c52da1acd50bbf460
seongjun yun <yunjy104@gmail.com>
2020-12-08 21:06:23 +0900

    한 서버에 두명이 들어가서 큐브를 움직이고 색깔을 바꿀수있다

---
+++ PhotonAPI/Assets/Photon/Scripts/CubeMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class CubeMovement : MonoBehaviour
+{
+    PhotonView m_PhotonView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PhotonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_PhotonView.IsMine)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.position += transform.up * Time.deltaTime;
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.position -= transform.up * Time.deltaTime;
+            if (Input.GetKey(KeyCode.RightArrow))
+                transform.position += transform.right * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.position -= transform.right * Time.deltaTime;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        m_PhotonView.RPC("ColorRandomSwitch", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+
+    public void ColorRandomSwitch()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.color = new Color(Random.Range(0, 1f),
+            Random.Range(0, 1f), Random.Range(0, 1f));
+    }
+
+
+}
---
+++ PhotonAPI/Assets/Photon/Scripts/MyPhotonNetwork.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MyPhotonNetwork : MonoBehaviourPunCallbacks
+{
+
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected");
+    }
+
+    public void OnClickConnect()
+    {
+        Photon.Pun.PhotonNetwork.ConnectUsingSettings();
+        //Photon.Pun.PhotonNetwork.GameVersion = "test";
+    }
+
+    public void OnClickDisconnect()
+    {
+        Photon.Pun.PhotonNetwork.Disconnect();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        PhotonNetwork.LoadLevel("Level_Test1");
+    }
+}
