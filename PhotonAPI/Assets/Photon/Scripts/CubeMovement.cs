9c9c3bb63dd3fd823ac73cdbfa6a12aef61bab2f
seongjun yun <yunjy104@gmail.com>
2020-12-09 21:11:30 +0900

    photontransformview를 지우고 수동으로 코드를 써서 설정함 그리고 온라인상에 색깔을바꿀때 상대방한테도 나타나게 할수있는 코드 작성

--- PhotonAPI/Assets/Photon/Scripts/CubeMovement.cs
+++ PhotonAPI/Assets/Photon/Scripts/CubeMovement.cs
@@ -14,9 +14,11 @@ public class CubeMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    private Vector3 m_OldPosition;
+
     void Update()
     {
-        if (m_PhotonView.IsMine)
+        if (m_PhotonView.IsMine || !m_PhotonView.IsMine)
         {
             if (Input.GetKey(KeyCode.UpArrow))
                 transform.position += transform.up * Time.deltaTime;
@@ -27,21 +29,56 @@ public class CubeMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow))
                 transform.position -= transform.right * Time.deltaTime;
         }
-    }
 
-    private void OnMouseDown()
-    {
-        m_PhotonView.RPC("ColorRandomSwitch", RpcTarget.AllBuffered);
+        if(m_OldPosition != transform.position)
+        {
+            m_PhotonView.RPC("Move", RpcTarget.AllBuffered, transform.position, transform.rotation);
+        }
+
+        m_OldPosition = transform.position;
     }
 
     [PunRPC]
 
-    public void ColorRandomSwitch()
+    public void Move(Vector3 pos, Quaternion rot)
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+        m_OldPosition = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        float r, g, b;
+
+        r = Random.Range(0, 1f);
+        g = Random.Range(0, 1f);
+        b = Random.Range(0, 1f);
+
+        m_PhotonView.RPC("ColorRandomSwitch", RpcTarget.AllBuffered, r, g, b);
+
+        //이렇게도 가능(ColorRandomSwitch2)를 써서 위에 코드와 똑같이 할수있다.
+        //Color color = new Color();
+        //color.r = Random.Range(0, 1f);
+        //color.g = Random.Range(0, 1f);
+        //color.b = Random.Range(0, 1f);
+
+        //m_PhotonView.RPC("ColorRandomSwitch", RpcTarget.AllBuffered, color);
+    }
+
+    [PunRPC]
+
+    public void ColorRandomSwitch(float r, float g, float b)
     {
         Renderer renderer = GetComponent<Renderer>();
-        renderer.material.color = new Color(Random.Range(0, 1f),
-            Random.Range(0, 1f), Random.Range(0, 1f));
+        renderer.material.color = new Color(r, g, b);
     }
+
+    //[PunRPC]
+    //public void ColorRandomSwitch2(object color)
+    //{
+    //    var _color = color as Color;
+    //}
 
 
 }
