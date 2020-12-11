70222d1c17098ceaa596f77dbbae9b2b578aafee
seongjun yun <yunjy104@gmail.com>
2020-12-11 21:10:27 +0900

    오목게임에 룰을 추가해서 간단하게 플레이할수있게 만들었다.

--- PhotonAPI/Assets/Photon/Scripts/FiveInARow/LevelScript_FiveInARow.cs
+++ PhotonAPI/Assets/Photon/Scripts/FiveInARow/LevelScript_FiveInARow.cs
@@ -15,7 +15,7 @@ public enum Turn
     Blue    //블루 턴
 }
 
-public class LevelScript_FiveInARow :MonoBehaviour
+public class LevelScript_FiveInARow : MonoBehaviour
 {
     public Turn m_Turn;
 
@@ -25,14 +25,14 @@ public class LevelScript_FiveInARow :MonoBehaviour
     public void Start()
     {
         var points = FindObjectsOfType<Point>();
-        foreach(var point in points)
+        foreach (var point in points)
         {
             m_Board[point.m_Point.x, point.m_Point.y]
                 = point;
 
             point.m_Side = Side.Other;
 
-            point.SetColor(1,1,1);
+            point.SetColor(1, 1, 1);
         }
     }
 
@@ -41,17 +41,22 @@ public class LevelScript_FiveInARow :MonoBehaviour
 
     public void LetGoOfTheHorse(Side side, Vector2Int pos)
     {
-        if((side == Side.Red && m_Turn == Turn.Red)
+        if ((side == Side.Red && m_Turn == Turn.Red)
             || (side == Side.Blue && m_Turn == Turn.Blue))
         {
-            if(m_Board[pos.x, pos.y].m_Side == Side.Other)
+            if (m_Board[pos.x, pos.y].m_Side == Side.Other)
             {
                 m_Board[pos.x, pos.y].m_Side = side;
 
-                if(side == Side.Red)
+                if (side == Side.Red)
                     m_Board[pos.x, pos.y].SetColor(1, 0, 0);
                 else
                     m_Board[pos.x, pos.y].SetColor(0, 0, 1);
+
+                if (CheckVictory(side, pos))
+                {
+                    Debug.Log("victory : " + side);
+                }
 
                 NextTurn();
             }
@@ -60,13 +65,197 @@ public class LevelScript_FiveInARow :MonoBehaviour
 
     public void NextTurn()
     {
-        if(m_Turn == Turn.Red)
+        if (m_Turn == Turn.Red)
         {
             m_Turn = Turn.Blue;
         }
-        else if(m_Turn == Turn.Blue)
+        else if (m_Turn == Turn.Blue)
         {
             m_Turn = Turn.Red;
         }
+    }
+
+    //수정
+    public bool CheckVictory(Side side, Vector2Int pos)
+    {
+        //삼방에
+
+        //상하에 대해서 체크
+        {
+            int rowCount = 1;
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x + i, pos.y))
+                {
+                    break;
+                }
+                if (m_Board[pos.x + i, pos.y].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x, pos.y - i))
+                {
+                    break;
+                }
+                if (m_Board[pos.x, pos.y - i].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            if (rowCount >= 5)
+            {
+                return true;
+            }
+        }
+
+        //좌우에 대해서 체크
+        {
+            int rowCount = 1;
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x + i, pos.y))
+                {
+                    break;
+                }
+                if (m_Board[pos.x + i, pos.y].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x - i, pos.y))
+                {
+                    break;
+                }
+                if (m_Board[pos.x - i, pos.y].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            if (rowCount >= 5)
+            {
+                return true;
+            }
+        }
+
+        //대각선에 대한 체크
+        {
+            int rowCount = 1;
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x + i, pos.y + i))
+                {
+                    break;
+                }
+                if (m_Board[pos.x + i, pos.y + i].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x - i, pos.y - i))
+                {
+                    break;
+                }
+                if (m_Board[pos.x - i, pos.y - i].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            if (rowCount >= 5)
+            {
+                return true;
+            }
+        }
+
+        //역대각선에 대한 체크
+        {
+            int rowCount = 1;
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x + i, pos.y - i))
+                {
+                    break;
+                }
+                if (m_Board[pos.x + i, pos.y - i].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            //위로 몇 번이나 반복되는지
+            for (int i = 1; i < 5; i++)
+            {
+                if (!InBoardRange(pos.x - i, pos.y + i))
+                {
+                    break;
+                }
+                if (m_Board[pos.x - i, pos.y + i].m_Side != side)
+                {
+                    break;
+                }
+
+                rowCount++;
+            }
+
+            if (rowCount >= 5)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool InBoardRange(int x, int y)
+    {
+        bool result = true;
+        if (x < 0 || x >= m_Board.GetLength(0))
+        {
+            result &= false;
+        }
+
+        if (y < 0 || y >= m_Board.GetLength(1))
+        {
+            result &= false;
+        }
+
+        return result;
     }
 }
--- PhotonAPI/Assets/Photon/Scripts/FiveInARow/Player_FiveARow.cs
+++ PhotonAPI/Assets/Photon/Scripts/FiveInARow/Player_FiveARow.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.EventSystems;
+
 public class Player_FiveARow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Side m_Side;
+    private LevelScript_FiveInARow m_LevelScript;
+
+    public void Awake()
     {
-        
+        m_LevelScript = FindObjectOfType<LevelScript_FiveInARow>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //UI에 대한 블럭처리
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                //카메라 오브젝트를 선택하려고 할 때에
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
+                {
+                    //내가 보드판에서 칸을 선택했다면
+                    if (hit.collider != null)
+                    {
+                        var point = hit.collider.GetComponent<Point>();
+                        if (point != null)
+                        {
+                            m_LevelScript.LetGoOfTheHorse(m_Side, point.m_Point);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
