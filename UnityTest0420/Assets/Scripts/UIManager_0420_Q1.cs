136fc46e06e60253f3df79361fe7a6c8ec75e441
seongjun yun <yunjy104@gmail.com>
2021-04-20 21:13:17 +0900

    오늘 공부했는데 인풋필드에 넣은 텍스트 드롭다운에 추가및 삭제하기

---
+++ UnityTest0420/Assets/Scripts/UIManager_0420_Q1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_0420_Q1 : MonoBehaviour
+{
+    public InputField inputtxt;
+    public Button addbtn;
+    public Button delbtn;
+    public Dropdown drop1;
+    private int st1;
+    private string st2;
+
+    void initialized()
+    {
+        drop1.options.Clear();
+        inputtxt.text = "";
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialized();
+        addbtn.onClick.AddListener(AddDropdown);
+        delbtn.onClick.AddListener(RemoveDropdown);
+        drop1.onValueChanged.AddListener(valuechanged1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void valuechanged1(int index)
+    {
+        st1 = index;
+    }
+    void AddDropdown()
+    {
+        Dropdown.OptionData a = new Dropdown.OptionData();
+        if (inputtxt.text != "")
+        {
+            a.text = inputtxt.text;
+        }
+
+        if (drop1.options.Contains(a) == false)
+        {
+            drop1.options.Add(a);
+            drop1.captionText.text = a.text;
+        }
+    }
+
+    void RemoveDropdown()
+    {
+        drop1.options.Remove(drop1.options[st1]);
+        drop1.RefreshShownValue();
+    }
+}
---
+++ UnityTest0420/Assets/Scripts/UIManager_Q4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_Q4 : MonoBehaviour
+{
+    public ToggleGroup tgall1;
+    public Button btn1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn1.onClick.AddListener(Click_Button);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Click_Button()
+    {
+
+    }
+}
